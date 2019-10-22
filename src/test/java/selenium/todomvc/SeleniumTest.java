0bd9c9fe392770110778b87cba8d7b56e34c7859
Safa Güner <safa.guner@getir.com>
2019-10-22 10:44:57 +0300

    Initial project setup

---
+++ src/test/java/selenium/todomvc/SeleniumTest.java
@@ -0,0 +1,15 @@
+package selenium.todomvc;
+
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@ExtendWith(SeleniumExtension.class)
+class SeleniumTest {
+
+    @Test
+    void projectIsConfigured(ChromeDriver driver) {
+        System.out.println("Test runned....");
+    }
+}
